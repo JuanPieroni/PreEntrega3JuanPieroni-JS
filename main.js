cf51b0164a34d144e2e80db5b7f1752324463ca2
Juan Pablo Pieroni <seisduro@gmail.com>
2023-02-27 18:34:35 -0300

    imagenes cargadas

---
+++ main.js
@@ -0,0 +1,10 @@
+class Productos {
+    constructor (item, nombre, precio, img) {
+        this.item = item;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img;
+        
+    }
+}
+
